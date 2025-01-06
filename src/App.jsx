e37b5bef8ee43ab7a28c8db679509bace30987f1
Sam Cherry <76022735+samuelcherry@users.noreply.github.com>
2025-01-06 11:43:24 -0700

    added login functionality

--- src/App.jsx
+++ src/App.jsx
@@ -2,15 +2,21 @@ import { Routes, Route } from "react-router-dom";
 import Home from "./components/Home";
 import Login from "./components/Login";
 import Register from "./components/Register";
+import { useState } from "react";
 
 export default function App() {
+  const [token, setToken] = useState("");
+  const [username, setUsername] = useState("");
   return (
     <div>
       <Routes>
         {/* Home Route */}
         <Route path="/" element={<Home />} />
         {/* Login Route */}
-        <Route path="/login" element={<Login />} />
+        <Route
+          path="/login"
+          element={<Login setToken={setToken} setUsername={setUsername} />}
+        />
         {/* Registration Route */}
         <Route path="/register" element={<Register />} />
       </Routes>
