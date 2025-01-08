c1310610315081fdb31940173c19a401581e15a9
Sam Cherry <76022735+samuelcherry@users.noreply.github.com>
2025-01-08 10:55:27 -0700

    added user posts and user page

---
+++ src/components/UserPage.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import NewPostForm from "./NewPostForm";
+import Feed from "./Feed";
+
+const UserPage = () => {
+  return (
+    <div>
+      <NewPostForm />
+      <Feed />
+    </div>
+  );
+};
+
+export default UserPage;
