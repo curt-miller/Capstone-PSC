8697c11fa2adeda2ed86ccb673019fe41ea950c7
Curt Miller <millercw3@gmail.com>
2025-01-09 18:47:49 -0500

    formatted the new post form

--- src/components/NewPostForm.jsx
+++ src/components/NewPostForm.jsx
@@ -52,50 +52,52 @@ export default function NewPostForm({ onPostSubmit }) {
   };
 
   return (
-    <div id="submit-page">
-      <div id="submit-form">
-        <h1>Drop a Pin!</h1>
-        <form onSubmit={handleSubmit}>
-          <div>
-            <label htmlFor="title">Title:</label>
-            <input
-              type="text"
-              id="title"
-              value={title}
-              onChange={(e) => setTitle(e.target.value)}
-              required
-            />
-          </div>
+    <div id="newpost-form">
+      <h1>Drop a Pin!</h1>
+      <form onSubmit={handleSubmit}>
 
-          <div>
-            <label htmlFor="description">
-              Description (140 characters max):
-            </label>
-            <textarea
-              id="description"
-              value={description}
-              onChange={(e) => setDescription(e.target.value)}
-              maxLength={140}
-              required
-            />
-          </div>
+        <div>
+          <label htmlFor="title">Title:</label>
+          <input
+            type="text"
+            id="title"
+            value={title}
+            onChange={(e) => setTitle(e.target.value)}
+            required
+          />
+        </div>
 
-          <div>
-            <label htmlFor="imageUrl">Image URL:</label>
-            <input
-              type="url"
-              id="imageUrl"
-              value={imageUrl}
-              onChange={(e) => setImageUrl(e.target.value)}
-            />
-          </div>
+        <div>
+          <label htmlFor="description">
+            Description :
+          </label>
+          <textarea
+            id="description"
+            value={description}
+            onChange={(e) => setDescription(e.target.value)}
+            maxLength={100}
+            placeholder="100 characters max"
+            required
+          />
+        </div>
 
-          <button type="submit">Submit</button>
-        </form>
-      </div>
-      <div>
-        <MapSearch onLocationChange={setLocation} location={location} />
-      </div>
+        <div>
+          <label htmlFor="imageUrl">Image URL:</label>
+          <input
+            type="url"
+            id="imageUrl"
+            value={imageUrl}
+            onChange={(e) => setImageUrl(e.target.value)}
+          />
+        </div>
+
+        <div id="newpost-map">
+          <MapSearch onLocationChange={setLocation} location={location} />
+        </div>
+
+        <button type="submit">Submit</button>
+      </form>
     </div>
   );
+
 }
