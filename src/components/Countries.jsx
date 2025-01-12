4db9c64bd90869ad95b8586987c5ac812d99e357
Priyanka Shirur <pshirur323@gmail.com>
2025-01-11 19:45:27 -0800

    home page grid conversion

--- src/components/Countries.jsx
+++ src/components/Countries.jsx
@@ -39,67 +39,72 @@ const Countries = ({ setCountry, refreshPosts }) => {
     <div>
       <h1 className="countries_header">Countries</h1>
 
-      {/* Alphabet Filter */}
-      <div className="alphabet_filter">
-        {[
-          "A",
-          ...Array.from({ length: 25 }, (_, i) => String.fromCharCode(66 + i))
-        ].map((letter) => (
-          <button
-            key={letter}
-            className={`alphabet_button ${
-              selectedLetter === letter ? "active" : ""
-            }`}
-            onClick={() => handleFilter(letter)}
-          >
-            {letter}
-          </button>
-        ))}
-      </div>
-
-      {/* Country Cards */}
-      {filteredCountries.length === 0 ? (
-        <p>No countries found</p>
-      ) : (
-        <div className="countries_container">
-          {filteredCountries.map((country, index) => (
-            <div className="country_card" key={index}>
-              <Link
-                to={{
-                  pathname: `/${country.name.toLowerCase()}`
-                }}
-                onClick={() => setCountry(country)}
-                className="country_name_link"
-              >
-                <h2 className="country_name">{country.name}</h2>
-              </Link>
-              <button className="like_country_button">
-                <img
-                  src={likeButton}
-                  alt="Like button Icon"
-                  className="like_button_icon"
-                  onClick={() => console.log("like button clicked")}
-                />{" "}
-              </button>
-              <button className="visited_country_button">
-                <img
-                  src={visitedButton}
-                  alt="Visited button Icon"
-                  className="visited_button_icon"
-                  onClick={() => console.log("visited button clicked")}
-                />{" "}
-              </button>
-              <p>Capital: {country.capital}</p>
-              <img
-                src={country.href.flag}
-                alt={`Flag of ${country.name}`}
-                className="country_flag"
-              />
-              <ImageGrid country={country.name} />
-            </div>
+      {/* Grid Layout Container */}
+      <div className="countries_grid">
+        {/* Alphabet Filter */}
+        <div className="alphabet_filter">
+          {[
+            "A",
+            ...Array.from({ length: 25 }, (_, i) =>
+              String.fromCharCode(66 + i)
+            ),
+          ].map((letter) => (
+            <button
+              key={letter}
+              className={`alphabet_button ${
+                selectedLetter === letter ? "active" : ""
+              }`}
+              onClick={() => handleFilter(letter)}
+            >
+              {letter}
+            </button>
           ))}
         </div>
-      )}
+
+        {/* Country Cards */}
+        <div className="countries_container">
+          {filteredCountries.length === 0 ? (
+            <p>No countries found</p>
+          ) : (
+            filteredCountries.map((country, index) => (
+              <div className="country_card" key={index}>
+                <Link
+                  to={{
+                    pathname: `/${country.name.toLowerCase()}`,
+                  }}
+                  onClick={() => setCountry(country)}
+                  className="country_name_link"
+                >
+                  <h2 className="country_name">{country.name}</h2>
+                </Link>
+                <button className="like_country_button">
+                  <img
+                    src={likeButton}
+                    alt="Like button Icon"
+                    className="like_button_icon"
+                    onClick={() => console.log("like button clicked")}
+                  />
+                </button>
+                <button className="visited_country_button">
+                  <img
+                    src={visitedButton}
+                    alt="Visited button Icon"
+                    className="visited_button_icon"
+                    onClick={() => console.log("visited button clicked")}
+                  />
+                </button>
+                <p>Capital: {country.capital}</p>
+                <img
+                  src={country.href.flag}
+                  alt={`Flag of ${country.name}`}
+                  className="country_flag"
+                />
+                <ImageGrid country={country.name} />
+              </div>
+            ))
+          )}
+        </div>
+      </div>
     </div>
   );
 };
